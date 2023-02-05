291262aa3e2be6ec090a7dd2ae618e407a33fe84
Kamal Yadav <kamalyadav122333@gmail.com>
2023-02-05 17:43:47 +0530

    updation

--- src/Calculator.js
+++ src/Calculator.js
@@ -32,17 +32,17 @@ function Calculator() {
           
          <input type="text"  value={input}/><br />
          
-         <button name="1" onClick={handelClick}  >7</button>
-         <button name="2" onClick={handelClick}  >8</button>
-         <button  name="3" onClick={handelClick} >9</button>
+         <button name="7" onClick={handelClick}  >7</button>
+         <button name="8" onClick={handelClick}  >8</button>
+         <button  name="9" onClick={handelClick} >9</button>
          <button    onClick={clr}>C</button><br />
          <button name="4" onClick={handelClick} >4</button>
          <button name="5" onClick={handelClick} >5</button>
          <button name="6" onClick={handelClick} >6</button>
          <button name="-"  onClick={handelClick} >-</button><br/>
-         <button name="7"  onClick={handelClick} >1</button>
-         <button name="8"  onClick={handelClick} >2</button>
-         <button name="9"  onClick={handelClick} >3</button>
+         <button name="1"  onClick={handelClick} >1</button>
+         <button name="2"  onClick={handelClick} >2</button>
+         <button name="3"  onClick={handelClick} >3</button>
          <button name="*"   onClick={handelClick} >X</button><br/>
          <button name="0"  onClick={handelClick} >0</button>
          <button name="." onClick={handelClick} >.</button>
